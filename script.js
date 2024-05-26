a12e8c030fa5f441acc0448bf9f514b0cce78ccf
Gustan021 <sugobw@gmail.com>
2024-05-26 01:07:10 -0300

    final

---
+++ script.js
@@ -0,0 +1,36 @@
+let num1 = parseFloat(prompt("Digite o primeiro número do cálculo"))
+let sinal = prompt("Digite o sinal. Ex +(soma), -()subtração...")
+let num2 = parseFloat(prompt("Digite o segundo número do cálculo"))
+
+function calculadora(num1, sinal, num2) {
+
+    if (sinal == "+") {
+        alert("O resultado é =" + (num1 + num2))
+    } else if (sinal == "-") {
+        alert("O resultado é =" + (num1 - num2))
+    } else if (sinal == "*") {
+        alert("O resultado é =" + (num1 * num2))
+    } else if (sinal == "/") {
+        alert("O resultado é =" + (num1 / num2))
+    }
+}
+
+calculadora(num1, sinal, num2)
+
+let continuar = prompt("Deseja realizar outro cálculo ? (sim/não)").toLowerCase()
+
+while (continuar) {
+    if (continuar === "sim") {
+        let num1 = parseFloat(prompt("Digite o primeiro número do cálculo"))
+        let sinal = prompt("Digite o sinal. Ex +(soma), -()subtração...")
+        let num2 = parseFloat(prompt("Digite o segundo número do cálculo"))
+
+        calculadora(num1, sinal, num2)
+
+        continuar = prompt("Deseja realizar outro cálculo ? (sim/não)").toLowerCase()
+    } else {
+        alert("obrigado por usar nossa calculadora aperte f5 quando quiser usar novamente")
+        break
+    }
+}
+
